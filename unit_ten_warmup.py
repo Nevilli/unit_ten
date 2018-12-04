b49ef6bc1617881183530cf860805aeba551cdaf
Lliam <lliam@iMac8.local>
2018-12-04 13:45:05 -0500

    Created rectangles for the logo

--- unit_ten_warmup.py
+++ unit_ten_warmup.py
@@ -1,10 +1,16 @@
 import pygame, sys
 from pygame.locals import*
+import logo
 
 
 pygame.init()
-main_surface = pygame.display.set_mode((500, 500), 0, 32)
+main_surface = pygame.display.set_mode((500, 250), 0, 32)
 pygame.display.set_caption("Sandy Spring Friends School")
+
+
+ssfs = logo.Logo(main_surface)
+ssfs.draw_rectangles()
+
 
 while True:
     for event in pygame.event.get():
