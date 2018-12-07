25272cc8c920ae9b65333f2a563eef00c9580763
Lliam <lliam@iMac8.local>
2018-12-07 08:56:57 -0500

    Created constants and for loop

--- class_file.py
+++ class_file.py
@@ -2,14 +2,33 @@ import brick
 import pygame, sys
 from pygame.locals import*
 
-# self.GREEN = (0, 102, 71)
-# self.RED = (255, 0, 0)
-# self.BLUE = (0, 0, 255)
-# self.GOLD = (255, 209, 63)
-# self.PURPLE = (255, 0, 255)
+GREEN = (0, 102, 71)
+RED = (255, 0, 0)
+BLUE = (0, 0, 255)
+GOLD = (255, 209, 63)
+PURPLE = (255, 0, 255)
+SPACE = 5
+HEIGHT = 20
+x_number_bricks = 9
+
 
 pygame.init()
 main_surface = pygame.display.set_mode((500, 250), 0, 32)
 pygame.display.set_caption("Brick pyramid")
 
-bricks = 
+WIDTH = (500 - (x_number_bricks * SPACE))/x_number_bricks
+
+x = 0
+y = 250 - HEIGHT
+
+for b in range(x_number_bricks):
+    bricks = brick.Brick(WIDTH, HEIGHT, GREEN, main_surface)
+    bricks.draw_brick(x, y)
+    pygame.display.update()
+
+
+while True:
+    for event in pygame.event.get():
+        if event.type == QUIT:
+            pygame.quit()
+            sys.exit()
